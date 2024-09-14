54fb7e72f6efe666766e16a282d05e282b01f1a1
Leon Jovanovic <leonjovanovic1996@gmail.com>
2024-09-14 22:26:13 +0200

    knowledge graph initial

--- document_pipeline.py
+++ document_pipeline.py
@@ -17,7 +17,7 @@ class DocumentPipeline:
     def __init__(self, config: dict) -> None:
         self._config = config
         self._preprocessing_pipeline = PreprocessingPipeline(
-            self._config["input_pipeline"]
+            self._config["preprocessing_pipeline"]
         )
         self._triplet_extraction_pipeline = TripletExtractionPipeline(
             self._config["triplet_extraction"]
@@ -25,19 +25,19 @@ class DocumentPipeline:
 
     def run(self):
         docs: list[Document] = self._preprocessing_pipeline.run(
-            self._config["storage"]["file_path"]
+            self._config["input"]["file_path"]
         )['document_splitter']['documents']
         logger.info(f"Document was split into {len(docs)} documents")
-        if bool(self._config["storage"]["load_saved_file"]):
+        if bool(self._config["input"]["load_saved_file"]):
             self.triplets = self._load_triplets(
-                self._config["storage"]["saved_file_path"]
+                self._config["input"]["saved_file_path"]
             )
         else:
             self.triplets = self._triplet_extraction_pipeline.run(
                 [doc.content for doc in docs]
             )
         logger.info(f"Total number of triplets: {len(self.triplets)}")
-        self._store(docs, self.triplets)
+        self._store(self._config["output"], docs, self.triplets)
         return self.triplets
 
     def _load_triplets(self, path: str) -> list[Triplet]:
@@ -46,12 +46,18 @@ class DocumentPipeline:
         self.triplets = [Triplet.from_dict(t) for t in processed_doc["triplets"]]
         return self.triplets
 
-    def _store(self, docs: list[Document], triplets: list[Triplet]) -> None:
+    def _store(self, config: dict, docs: list[Document], triplets: list[Triplet]) -> None:
+        if not config["store_output"]:
+            return
+        
         storage = {
             "name": os.path.basename(docs[0].meta["file_path"]),
             "num_pages": docs[-1].meta["page_number"],
             "config": self._config,
             "triplets": [t.to_dict() for t in triplets],
         }
+        
+        if not os.path.exists(config["folder_path"]):
+            os.makedirs(config["folder_path"])
         with open(f"storage/{str(uuid4())}.json", "w") as f:
             json.dump(storage, f, indent=4)
---
+++ graphs/knowledge_graph/knowledge_graph.py
@@ -0,0 +1,49 @@
+import os
+from neo4j import GraphDatabase
+
+from triplet_extraction.classes import Entity, Triplet
+
+
+class Neo4JGraph:
+    def __init__(self) -> None:
+        self._driver = self._connect()
+
+    def populate(self, triplets: list[Triplet]):
+        with self._driver.session() as session:
+            for triplet in triplets:                
+                # Create subject and object entities
+                session.write_transaction(Neo4JGraph.create_entity, triplet.subject])
+                session.write_transaction(Neo4JGraph.create_entity, triplet.object)                
+                # Create relationship
+                session.write_transaction(Neo4JGraph.create_relationship, triplet)
+
+    def open_connection(self):
+        self._driver = self._connect()
+
+    def close_connection(self):
+        self._driver.close()
+    
+    @staticmethod
+    def _connect():
+        uri = os.environ["NEO4J_URI"]
+        username = os.environ["NEO4J_USERNAME"]
+        password = os.environ["NEO4J_PASSWORD"]
+        return GraphDatabase.driver(uri, auth=(username, password))
+    
+    @staticmethod
+    def create_entity(tx, entity: Entity):
+        query = (
+            f"CREATE (e:{entity.type} {{name: $name, description: $description}}) "
+            "RETURN e"
+        )
+        tx.run(query, name=entity.name, description=entity.description)
+
+    @staticmethod
+    def create_relationship(tx, triplet: Triplet):
+        query = (
+            f"MATCH (s:{triplet.subject.type} {{name: $subject_name}}), (o:{triplet.object.type} {{name: $object_name}}) "
+            f"MERGE (s)-[r:{triplet.relationship.description} {{strength: $strength}}]->(o) "
+            "RETURN s, r, o"
+        )
+        tx.run(query, subject_name=triplet.subject.name, object_name=triplet.object.name, strength=triplet.relationship.strength)
+
